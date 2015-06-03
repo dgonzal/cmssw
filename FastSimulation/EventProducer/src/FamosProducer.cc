a60ccb8a6eeeef81fcbae11a5b4a105708ebb7fc
Lukas <lukas.vanelderen@cern.ch>
2015-06-03 20:24:51 +0200

    fix type

--- FastSimulation/EventProducer/src/FamosProducer.cc
+++ FastSimulation/EventProducer/src/FamosProducer.cc
@@ -95,7 +95,7 @@ void FamosProducer::produce(edm::Event & iEvent, const edm::EventSetup & es)
    std::auto_ptr<edm::SimTrackContainer> p1(new edm::SimTrackContainer);
    std::auto_ptr<edm::SimTrackContainer> m1(new edm::SimTrackContainer);
    std::auto_ptr<edm::SimVertexContainer> p2(new edm::SimVertexContainer);
-   //std::auto_ptr<FSimVertexTypeCollection> v1(new FSimVertexTypeCollection);
+   std::auto_ptr<FSimVertexTypeCollection> v1(new FSimVertexTypeCollection);
    //std::auto_ptr<edm::PSimHitContainer> p3(new edm::PSimHitContainer);
    std::auto_ptr<edm::PCaloHitContainer> p4(new edm::PCaloHitContainer);
    std::auto_ptr<edm::PCaloHitContainer> p5(new edm::PCaloHitContainer);
