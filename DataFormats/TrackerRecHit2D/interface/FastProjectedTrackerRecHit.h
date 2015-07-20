377ff88d81569e3e380affbba6ffdc1f72bfa0c1
Lukas <lukas.vanelderen@cern.ch>
2015-07-20 10:45:33 +0200

    introduce a container for single fastsim rechits (not matched, not projected)

--- DataFormats/TrackerRecHit2D/interface/FastProjectedTrackerRecHit.h
+++ DataFormats/TrackerRecHit2D/interface/FastProjectedTrackerRecHit.h
@@ -2,6 +2,7 @@
 #define FastProjectedTrackerRecHit_H
 
 #include "DataFormats/TrackerRecHit2D/interface/FastTrackerRecHit.h"
+#include "DataFormats/TrackerRecHit2D/interface/FastSingleTrackerRecHit.h"
 #include "DataFormats/TrackerRecHit2D/interface/ProjectedSiStripRecHit2D.h"
 
 class FastProjectedTrackerRecHit : public FastTrackerRecHit {
@@ -15,7 +16,7 @@ class FastProjectedTrackerRecHit : public FastTrackerRecHit {
     FastProjectedTrackerRecHit( const LocalPoint& pos, 
 				const LocalError& err, 
 				GeomDet const & idet,
-				FastTrackerRecHit const & originalHit) 
+				FastSingleTrackerRecHit const & originalHit) 
 	: FastTrackerRecHit(pos, err, idet, 
 			    ProjectedSiStripRecHit2D::isMono(idet,*originalHit.det()) 
 			    ? fastTrackerRecHitType::siStripProjectedMono2D 
@@ -23,14 +24,17 @@ class FastProjectedTrackerRecHit : public FastTrackerRecHit {
 	, originalHit_(originalHit)
     {}
     
-    virtual bool sharesInput (const TrackingRecHit* other, SharedInputType what) const {return originalHit_.sharesInput(other,what);}
+    bool sharesInput (const TrackingRecHit* other, SharedInputType what) const {return originalHit_.sharesInput(other,what);}
     const FastTrackerRecHit & originalHit() const {return originalHit_;}
-    virtual FastTrackerRecHit * clone() const {FastProjectedTrackerRecHit * p =  new FastProjectedTrackerRecHit( * this); p->load(); return p;}
-
+    virtual FastProjectedTrackerRecHit * clone() const {FastProjectedTrackerRecHit * p =  new FastProjectedTrackerRecHit( * this); p->load(); return p;}
+    size_t                       nSimTrackIds()            const { return originalHit_.nSimTrackIds();}                             ///< see addSimTrackId(int32_t simTrackId)
+    int32_t                      simTrackId(size_t i)      const { return originalHit_.simTrackId(i);}                              ///< see addSimTrackId(int32_t simTrackId)
+    int32_t                      simTrackEventId(size_t i) const { return originalHit_.simTrackEventId(i);;}                        ///< see addSimTrackId(int32_t simTrackId)
+    
     private:
 
-    FastTrackerRecHit originalHit_;
-
+    FastSingleTrackerRecHit originalHit_;
+    
 };
 
 #endif
